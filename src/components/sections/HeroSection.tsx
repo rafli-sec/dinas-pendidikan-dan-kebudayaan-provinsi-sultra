4f27c09b6eb524eef9052cd7fb549477e427f1a7
aziscout <abdulaziscout@gmail.com>
2025-09-14 17:36:12 +0800

    Update hero-section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -4,7 +4,7 @@ import heroImage from "@/assets/hero-education.jpg";
 
 const HeroSection = () => {
   return (
-    <section className="relative min-h-[600px] flex items-center overflow-hidden">
+    <section className="relative h-screen flex items-center overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <img
@@ -14,13 +14,11 @@ const HeroSection = () => {
         />
         <div className="absolute inset-0 bg-gradient-to-r from-government-blue/90 via-government-blue/70 to-transparent"></div>
       </div>
-
       {/* Content */}
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
-        <div className="max-w-3xl">
-          <div className="flex items-center space-x-3 mb-6">
-            <div className="w-12 h-12  backdrop-blur-sm rounded-lg flex items-center justify-center">
-              {/* <GraduationCap className="w-6 h-6 text-white" /> */}
+        <div className="max-w-3xl text-center mx-auto">
+          <div className="flex items-center justify-center space-x-3 mb-6">
+            <div className="w-12 h-12 backdrop-blur-sm rounded-lg flex items-center justify-center">
               <img src="../../../public/logo.png" alt="logo sultra" />
             </div>
             <div className="text-white/90">
@@ -34,13 +32,13 @@ const HeroSection = () => {
             <span className="block text-gold">Sulawesi Tenggara</span>
           </h1>
 
-          <p className="text-xl text-white/90 mb-8 leading-relaxed max-w-2xl">
+          <p className="text-xl text-white/90 mb-8 leading-relaxed max-w-2xl mx-auto">
             Membangun pendidikan berkualitas untuk masa depan yang gemilang.
             Melayani masyarakat dengan dedikasi dan inovasi dalam mengembangkan
             sistem pendidikan di Sulawesi Tenggara.
           </p>
 
-          <div className="flex flex-col sm:flex-row gap-4">
+          <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <Button
               size="lg"
               className="bg-white text-government-blue hover:bg-white/90 shadow-medium"
@@ -51,7 +49,7 @@ const HeroSection = () => {
             <Button
               variant="outline"
               size="lg"
-              className="border-white   hover:text-white  bg-white/10 text-white border-2"
+              className="border-white hover:text-white bg-white/10 text-white border-2"
             >
               Profil Dinas
             </Button>
@@ -79,6 +77,7 @@ const HeroSection = () => {
       <div className="absolute top-20 right-20 w-32 h-32 bg-white/5 rounded-full blur-xl"></div>
       <div className="absolute bottom-40 right-40 w-20 h-20 bg-gold/20 rounded-full blur-lg"></div>
     </section>
+
   );
 };
 
